035be6c86efc0af3443d2143ad24677432fcc972
Kashish <kashish11chugh@gmail.com>
2021-12-06 23:39:41 +0530

    added initial files

---
+++ src/test/java/com/example/PPMtool/PersonalProjectManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PPMtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProjectManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
